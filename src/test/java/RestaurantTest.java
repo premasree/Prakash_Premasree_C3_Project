ef178a7a322fe12488c868054ca76222458e1336
premasree <premasreeprakash@gmail.com>
2022-07-20 12:34:16 -0400

    Part 3: Solution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -2,10 +2,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 class RestaurantTest {
     Restaurant restaurant;
@@ -72,4 +75,31 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    @Test
+    public void get_the_price_of_single_item() {
+        int expected_price = 119;
+        int actual_price = restaurant.getItemPrice("Sweet corn soup");
+        assertEquals(expected_price, actual_price);
+    }
+
+    @Test
+    public void get_the_price_of_single_item_not_present() {
+        int expected_price = 0;
+        int actual_price = restaurant.getItemPrice("Dummy Dish");
+        assertEquals(expected_price, actual_price);
+    }
+
+    @Test
+    public void get_the_total_price_for_items_selected() {
+        int expected_total_price = 119 + 269 ; //price of sweet corn soup and veg lasagne
+
+        List<String> itemList = new ArrayList<>();
+
+        itemList.add("Sweet corn soup");
+        itemList.add("Vegetable lasagne");
+
+        int actual_total_price = restaurant.getTotalPriceForItems(itemList);
+        assertEquals(expected_total_price, actual_total_price);
+    }
 }
